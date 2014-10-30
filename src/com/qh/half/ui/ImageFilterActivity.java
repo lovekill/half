a280856228d283c7fbd0340fd029d157972467d9
unknown <lihongbo2011@gmail.com>
2014-10-30 13:58:56 +0800

    add image

--- src/com/qh/half/ui/ImageFilterActivity.java
+++ src/com/qh/half/ui/ImageFilterActivity.java
@@ -3,7 +3,9 @@ package com.qh.half.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -15,10 +17,12 @@ import cn.Ragnarok.BitmapFilter;
 import com.qh.half.R;
 import com.qh.half.util.ImageUtil;
 
+import java.io.IOException;
+
 /**
  * Created by USER on 2014/10/29.
  */
-public class ImageFilterActivity extends BaseActivity implements View.OnClickListener{
+public class ImageFilterActivity extends BaseActivity implements View.OnClickListener {
     @InjectView(R.id.close)
     ImageView mClose;
     @InjectView(R.id.camaraPreView)
@@ -32,18 +36,52 @@ public class ImageFilterActivity extends BaseActivity implements View.OnClickLis
     private boolean isLeft = true;
     private static String PATH = "imagePath";
     private static String ISLEFT = "isleft";
+    private Bitmap changeBitmap;
+    private Bitmap originBitmap;
+    private String path = "";
+    int degree = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter_half);
         ButterKnife.inject(this);
+        path = getIntent().getStringExtra(PATH);
+        try {
+            ExifInterface exifInterface = new ExifInterface(path);
+            int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    degree = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    degree = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    degree = 270;
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         int size = getResources().getDimensionPixelSize(R.dimen.image_decode);
-        mCamaraPreView.setImageBitmap(ImageUtil.decodeSampledBitmapFromFile(getIntent().getStringExtra(PATH),size,size));
-        if(!getIntent().getBooleanExtra(ISLEFT,true)){
+//        mCamaraPreView.setImageBitmap(ImageUtil.decodeSampledBitmapFromFile(path,size,size));
+        Bitmap bitmap = ImageUtil.decodeSampledBitmapFromFile(path, size, size);
+        if (degree > 0) {
+            Matrix m = new Matrix();
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            m.setRotate(degree); // 旋转angle度
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, width, height,
+                    m, true);// 从新生成图片
+        }
+        mCamaraPreView.setImageBitmap(bitmap);
+        if (!getIntent().getBooleanExtra(ISLEFT, true)) {
             mOrLayout.setVisibility(View.GONE);
             mVeLayout.setVisibility(View.VISIBLE);
         }
+        mHe0.setOnClickListener(this);
     }
 
     public static void startImageFilter(Context context, String imagepath, boolean isLeft) {
@@ -53,17 +91,64 @@ public class ImageFilterActivity extends BaseActivity implements View.OnClickLis
         context.startActivity(intent);
     }
 
-    public void change(){
-        Bitmap b =((BitmapDrawable) mCamaraPreView.getDrawable()).getBitmap();
-       Bitmap fiBit =  BitmapFilter.changeStyle(b,BitmapFilter.SOFT_GLOW_STYLE,0.6);
-        mCamaraPreView.setImageBitmap(fiBit);
+    public void change() {
+        originBitmap = ((BitmapDrawable) mCamaraPreView.getDrawable()).getBitmap();
+//        Bitmap fiBit = BitmapFilter.changeStyle(b, BitmapFilter.SOFT_GLOW_STYLE, 0.6);
+//        mCamaraPreView.setImageBitmap(fiBit);
+        if (changeBitmap != null) {
+            changeBitmap.recycle();
+            changeBitmap = null;
+        }
+
+        applyStyle(16);
     }
+
     @Override
     public void onClick(View v) {
-       switch (v.getId()){
-           case R.id.he0:
-               change();
-               break;
-       }
+        switch (v.getId()) {
+            case R.id.he0:
+                change();
+                break;
+        }
     }
+
+    private void applyStyle(int styleNo) {
+        switch (styleNo) {
+            case BitmapFilter.AVERAGE_BLUR_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.AVERAGE_BLUR_STYLE, 5); // maskSize, must odd
+                break;
+            case BitmapFilter.GAUSSIAN_BLUR_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.GAUSSIAN_BLUR_STYLE, 1.2); // sigma
+                break;
+            case BitmapFilter.SOFT_GLOW_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.SOFT_GLOW_STYLE, 0.6);
+                break;
+            case BitmapFilter.LIGHT_STYLE:
+                int width = originBitmap.getWidth();
+                int height = originBitmap.getHeight();
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.LIGHT_STYLE, width / 3, height / 2, width / 2);
+                break;
+            case BitmapFilter.LOMO_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.LOMO_STYLE,
+                        (originBitmap.getWidth() / 2.0) * 95 / 100.0);
+                break;
+            case BitmapFilter.NEON_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.NEON_STYLE, 200, 100, 50);
+                break;
+            case BitmapFilter.PIXELATE_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.PIXELATE_STYLE, 10);
+                break;
+            case BitmapFilter.MOTION_BLUR_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.MOTION_BLUR_STYLE, 10, 1);
+                break;
+            case BitmapFilter.OIL_STYLE:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, BitmapFilter.OIL_STYLE, 5);
+                break;
+            default:
+                changeBitmap = BitmapFilter.changeStyle(originBitmap, styleNo);
+                break;
+        }
+        mCamaraPreView.setImageBitmap(changeBitmap);
+    }
+
 }
